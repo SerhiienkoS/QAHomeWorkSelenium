ef8dc8b5ab62635b3028ce0c9b6af4fa78b0fc71
SerhiienkoS <sergey.sergienkow@gmail.com>
2025-09-08 23:19:06 +0200

    HW_findElements

--- src/test/java/com/ait/HomeWorkSelenium/FirstSeleniumTest.java
+++ src/test/java/com/ait/HomeWorkSelenium/FirstSeleniumTest.java
@@ -1,5 +1,6 @@
 package com.ait.HomeWorkSelenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -18,6 +19,42 @@ WebDriver driver;
     @Test
     public void openGoogleTest(){
         System.out.println("Google!!!!");
+    }
+
+    @Test
+    public void findTenElementsOnDemoWebShop() {
+        // Переходим на страницу из задания
+        driver.get("https://demowebshop.tricentis.com");
+
+        // 1) Лого в шапке (картинка)
+        driver.findElement(By.cssSelector(".header-logo img"));
+
+        // 2) Ссылка Register
+        driver.findElement(By.linkText("Register"));
+
+        // 3) Ссылка Log in
+        driver.findElement(By.linkText("Log in"));
+
+        // 4) Корзина (узел <a> внутри #topcartlink)
+        driver.findElement(By.cssSelector("#topcartlink a"));
+
+        // 5) Поле поиска (id)
+        driver.findElement(By.id("small-searchterms"));
+
+        // 6) Кнопка поиска (класс)
+        driver.findElement(By.cssSelector("input.search-box-button"));
+
+        // 7) Пункт верхнего меню "Books"
+        driver.findElement(By.cssSelector(".top-menu a[href='/books']"));
+
+        // 8) Заголовок блока Featured products
+        driver.findElement(By.xpath("//div[@class='title']/strong[text()='Featured products']"));
+
+        // 9) Поле ввода email для рассылки (newsletter)
+        driver.findElement(By.id("newsletter-email"));
+
+        // 10) Кнопка голосования в опросе (poll)
+        driver.findElement(By.id("vote-poll-1"));
     }
     //after - tearDown
     @AfterMethod(enabled = true)
